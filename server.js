013367cd02d09f13763c628947012a2a80e476cf
Oxnooo <mikulectom76@gmail.com>
2021-12-15 18:07:14 +0100

    Ajout des dossiers views et www

--- server.js
+++ server.js
@@ -11,13 +11,10 @@ let myobject = {
 }
 
 app.use( '/views',express.static(__dirname + '/views')); //redirectviews
-app.use('/js',express.static( __dirname + '/node_modules/jquery/dist'));
-app.use('/js',express.static( __dirname + '/node_modules/@popperjs/core/dist/umd'));
 app.use(express.static(__dirname + '/www'));
 
 app.use(bodyparser.urlencoded({extended : false }));
 app.use(bodyparser.json());
-
 app.listen(port, () => {
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
