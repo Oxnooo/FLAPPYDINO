8be1dc1ed68f1470b6a4d6e85b7abd5f4111e748
Audrey Thierry <thierau1@ur.univ-rouen.fr>
2021-12-15 18:35:05 +0100

    Modification du lien image et ajout du background animé

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const img = new Image();
-img.src = "images/flappy-bird-set.png";
+img.src = "../images/flappy-bird-set.png";
 
 // variables
 let gamePlaying = false;
@@ -18,7 +18,7 @@ let index = 0,
     currentScore, 
     pipe;
 
-// paramétre des pipes
+// paramètre des pipes
 const pipeWidth = 78;
 const pipeGap = 270;
 const pipeLoc = () => (Math.random() * ((canvas.height - (pipeGap + pipeWidth)) - pipeWidth)) + pipeWidth;
@@ -30,6 +30,15 @@ const setup = () => {
   // initialisation du flyHeight
   flyHeight = (canvas.height / 2) - (size[1] / 2);
 
-  // paramétrage des 3 premiers pipes
+  // paramètrage des 3 premiers pipes
   pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
 }
+const render = () => {
+    index++;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -(index * (speed / 2)) % canvas.width, 0, canvas.width, canvas.height);
+
+    window.requestAnimationFrame(render);
+}
+setup();
+img.onload = render;
