d8070ff95891b486efe0a9b45a1ea10c8a900375
Audrey Thierry <thierau1@ur.univ-rouen.fr>
2022-01-05 08:45:43 +0100

    Ajout du Player et des obstacles

--- www/js/script.js
+++ www/js/script.js
@@ -37,7 +37,38 @@ const render = () => {
     index++;
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -(index * (speed / 2)) % canvas.width, 0, canvas.width, canvas.height);
-
+    if (gamePlaying){
+      pipes.map(pipe => {
+        pipe[0] -= speed;
+        ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+        ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
+        if(pipe[0] <= -pipeWidth){
+          currentScore++;
+          bestScore = Math.max(bestScore, currentScore);
+          pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc()]];
+          console.log(pipes);
+        }
+        if ([
+          pipe[0] <= cTenth + size[0], 
+          pipe[0] + pipeWidth >= cTenth, 
+          pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1]
+        ].every(elem => elem)) {
+          gamePlaying = false;
+          setup();
+        }
+      })
+    }
+    if (gamePlaying) {
+      ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size);
+      flight += gravity;
+      flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+    } else {
+      ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
+      flyHeight = (canvas.height / 2) - (size[1] / 2);
+      ctx.fillText(`Meilleur score : ${bestScore}`, 55, 245);
+      ctx.fillText('Cliquez pour jouer', 48, 535);
+      ctx.font = "bold 30px courier";
+    }
     document.getElementById('bestScore').innerHTML = `Meilleur : ${bestScore}`;
     document.getElementById('currentScore').innerHTML = `Actuel : ${currentScore}`;
 
